11a42762da9630b1bddcd60262234e8bbbf59141
GAS1337 <noobtrooper40@gmail.com>
2026-01-22 00:07:12 +0100

    Bisland wendigster Truck, Rückwärtsgang gefickt

--- code/RotationControl.cs
+++ code/RotationControl.cs
@@ -14,14 +14,33 @@ public sealed class RotationControl : Component
 
 		if ( groundCheck.Hit )
 		{
-			CarBody.WorldRotation = CarBody.WorldRotation.Angles().WithRoll( CarBody.WorldRotation.Roll() / 1.5f );
+			// CarBody.WorldRotation = CarBody.WorldRotation.Angles().WithRoll( CarBody.WorldRotation.Roll() / 1.5f );
+
+			if ( Input.Down( "Left" ) )
+			{
+				Log.Info(CarBody.Velocity.Length);
+				CarBody.AngularVelocity += CarBody.WorldRotation.Up * 0.15f;
+				CarBody.Velocity += (CarBody.WorldRotation.Right) * CarBody.Velocity.Length.Remap(0, 5000, 0, 50);
+				Rotation rot = Rotation.From( CarBody.WorldRotation.Pitch(), CarBody.WorldRotation.Yaw() + 170, CarBody.WorldRotation.Roll() );
+				// CarBody.SmoothRotate(rot, 1f, Time.Delta); 
+			}
+			else if ( Input.Down( "Right" ) )
+			{
+				CarBody.AngularVelocity += CarBody.WorldRotation.Down * 0.15f;
+				CarBody.Velocity += (CarBody.WorldRotation.Left) * CarBody.Velocity.Length.Remap( 0, 5000, 0, 50 );
+				Rotation rot = Rotation.From( CarBody.WorldRotation.Pitch(), CarBody.WorldRotation.Yaw() - 170, CarBody.WorldRotation.Roll() );
+				// CarBody.SmoothRotate( rot, 1f, Time.Delta );
+			}
 		}
 		else 
 		{
+			// CarBody.AngularVelocity = CarBody.AngularVelocity.WithFriction( 0.01f );
+
 			if ( Input.Down( "Forward" ) )
 			{
-				CarBody.AngularVelocity += CarBody.WorldRotation.Right * 0.05f;
+				CarBody.AngularVelocity += CarBody.WorldRotation.Right * CarBody.Velocity.Length.Remap( 0, 5000, 0f, 0.3f );
 			}
+
 		}
 
 	}
