36dcd445fc1bc49507d46d506d38ce580095a81c
GAS1337 <noobtrooper40@gmail.com>
2026-03-02 22:16:33 +0100

    Added Fog + Camera Spazz gefixed

--- code/OrbitalCameraController.cs
+++ code/OrbitalCameraController.cs
@@ -8,6 +8,7 @@ public sealed class OrbitalCameraController : Component
 
 	[Property, Description( "Starting distance" )] int TargetDistanceToPlayer = 2000;
 	[Property, Description("Starting distance")] int DistanceToPlayer = 2000;
+	[Property, Description( "Vertical Offset" )] int VerticalOffset = 32;
 	[Property, Description( "Minimal distance to player" )] int MinDistanceToPlayer = 600;
 	[Property, Description( "Maximal distance to player" )] int MaxDistanceToPlayer = 10000;
 	[Property, Description( "Units one step zooms" )] int ZoomStrength = 200;
@@ -25,22 +26,30 @@ public sealed class OrbitalCameraController : Component
 		Rotation rotation = Rotation.From( Pitch, Yaw, 0 );
 
 		SceneTraceResult checkingSightline = Scene.Trace
-			.Ray( MainCamera.WorldPosition, Player.WorldPosition + Vector3.Up * 32 )
+			.Ray( MainCamera.WorldPosition, Player.WorldPosition + Vector3.Up * VerticalOffset )
 			.Radius(1)
 			.IgnoreGameObjectHierarchy(GameObject) // Ignores itself. Use tags depending on your setup
 			.Run();
 		// DebugOverlay.Trace( checkingSightline );
 
+		// Checks for objects around cam to avoid stuttering
+		SceneTraceResult checkingBehind = Scene.Trace
+			.Sphere( AutoZoomStrength * 2, MainCamera.WorldPosition, MainCamera.WorldPosition )
+			.IgnoreGameObjectHierarchy( GameObject ) // Ignores itself. Use tags depending on your setup
+			.Run();
+		// DebugOverlay.Trace( checkingBehind );
+
 		if ( Input.MouseWheel.y < 0 ) { TargetDistanceToPlayer = Math.Min( TargetDistanceToPlayer + ZoomStrength, MaxDistanceToPlayer ); }
 		if ( Input.MouseWheel.y > 0 ) { TargetDistanceToPlayer = Math.Max( TargetDistanceToPlayer - ZoomStrength, MinDistanceToPlayer); }
 
-		switch ((TargetDistanceToPlayer, checkingSightline.Distance, checkingSightline.Hit ))
+		switch ((TargetDistanceToPlayer, checkingSightline.Distance, checkingSightline.Hit, checkingBehind.Hit ))
 		{
 			default: break;
-			case( > 0, >0, false) when TargetDistanceToPlayer > checkingSightline.Distance + AutoZoomStrength:
+			// Nur wenn checkingBehind false soll distance mit autozoom größer werden
+			case( > 0, > 0, false, false ) when TargetDistanceToPlayer > checkingSightline.Distance + AutoZoomStrength:
 				DistanceToPlayer += AutoZoomStrength;
 				break;
-			case ( > 0, > 0, false ) when TargetDistanceToPlayer < checkingSightline.Distance - AutoZoomStrength:
+			case ( > 0, > 0, false, _ ) when TargetDistanceToPlayer < checkingSightline.Distance - AutoZoomStrength:
 				DistanceToPlayer -= AutoZoomStrength;
 				break;
 		}
@@ -50,7 +59,7 @@ public sealed class OrbitalCameraController : Component
 		}
 
 		// Apply Position and Rotation
-		MainCamera.WorldPosition = Player.WorldPosition + Vector3.Up * 32 - rotation.Forward * DistanceToPlayer; 
+		MainCamera.WorldPosition = Player.WorldPosition + Vector3.Up * VerticalOffset - rotation.Forward * DistanceToPlayer; 
 		MainCamera.WorldRotation = rotation;
 		
 	}
