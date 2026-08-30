2d57fbb8960e792eb9a18d63f287aed8f510ce1d
GAS1337 <noobtrooper40@gmail.com>
2026-08-30 21:38:44 +0200

    moa Achievements

--- code/HighscoreManager.cs
+++ code/HighscoreManager.cs
@@ -30,6 +30,32 @@ public sealed class HighscoreManager : Component
 		amount *= CalculateMultiplier();
 		amount = (float)Math.Round(amount, 0);
 		CurrentScore += amount;
+		Sandbox.Services.Stats.SetValue( "points_scored", CurrentScore );
+		switch (CurrentScore)
+		{
+			case >= 1000000:
+				Sandbox.Services.Achievements.Unlock( "one_million_points" );
+				break;
+			case >= 666666:
+				Sandbox.Services.Achievements.Unlock( "six_hundred_thousand_points" );
+				break;
+			case >= 333333:
+				Sandbox.Services.Achievements.Unlock( "three_hundredthousand_points" );
+				break;
+			case >= 100000:
+				Sandbox.Services.Achievements.Unlock( "one_hundred_thousand_points" );
+				break;
+			case >= 50000:
+				Sandbox.Services.Achievements.Unlock( "fifty_thousand_points" );
+				break;
+			case >= 10000:
+				Sandbox.Services.Achievements.Unlock( "ten_thousand_points" );
+				break;
+			
+			default:
+				break;
+
+		}
 		LastGainedScore = amount;
 		Log.Info("Score increased by " + amount + ". Current score: " + CurrentScore + " Last Gained Score: " + LastGainedScore);
 	}
@@ -60,6 +86,7 @@ public sealed class HighscoreManager : Component
 	{
 		// add score to leaderboard
 		Sandbox.Services.Stats.SetValue("LeaderboardTest", CurrentScore );
+		Sandbox.Services.Achievements.Unlock( "first_leaderboard_entry" );
 		Sandbox.Services.Stats.SetValue( "TimeTest", SinceGameStart );
 		LatestScore = CurrentScore;
 	}
